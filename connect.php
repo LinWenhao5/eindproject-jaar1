bfbf3bdb7e9e294c77faa86692ca6c86a7558793
Lin Wenhao <linwenhao5@gmail.com>
2022-02-10 11:12:11 +0100

    ....

--- connect.php
+++ connect.php
@@ -1,20 +1,20 @@
 <?php
 
-$host = 'localhost';
-$db = 'nsp';
-$user = 'nsp';
-$password = 'wart1drex*LIY2rern';
-$charset = 'utf8mb4';
-$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
-$pdo = new PDO($dsn, $user, $password);
-
-
 // $host = 'localhost';
 // $db = 'nsp';
-// $user = 'root';
-// $password = '';
+// $user = 'nsp';
+// $password = 'wart1drex*LIY2rern';
 // $charset = 'utf8mb4';
 // $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
 // $pdo = new PDO($dsn, $user, $password);
+
+
+$host = 'localhost';
+$db = 'nsp';
+$user = 'root';
+$password = '';
+$charset = 'utf8mb4';
+$dsn = "mysql:host=$host;dbname=$db;charset=$charset";
+$pdo = new PDO($dsn, $user, $password);
 ?>
 
